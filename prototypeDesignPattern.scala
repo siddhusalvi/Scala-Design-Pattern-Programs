cb3a9e98a55fc262cb6b868f159d1e0021eea8e4
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-19 11:51:36 +0530

    ADDED: Prototype design pattern program

---
+++ prototypeDesignPattern.scala
@@ -0,0 +1,42 @@
+/*
+Filename: singleton_thread_safe
+Created: Siddhesh Salvi
+Change history:19.2.2020 / Siddhesh Salvi
+Created: Siddhesh Salvi
+3. Prototype design pattern is used when the Object creation is a costly affair and
+requires a lot of time and resources and you have a similar object already existing.
+Use Prototype Pattern as shown in the Link above to create multiple Employee Object
+*/
+
+
+case class Employees(data:List[String])
+
+object prototypeDesignPattern {
+  def main(args: Array[String]): Unit = {
+    var flag = true
+    while(flag){
+      try{
+
+
+        var employees_obj = Employees(List("sunny","vaibhav","siddhu","rinku"))
+        var copy1 = employees_obj.copy()
+        var copy2 = employees_obj.copy()
+        var copy3 = employees_obj.copy()
+
+
+        System.out.println(s"The prototype is: ")
+        System.out.println(s"Copy is  1: ${copy1}")
+        System.out.println(s"Copy is : ${copy2}")
+        System.out.println(s"Copy is : ${copy3}")
+        System.out.println(s"1 and 2 are equal: ${copy1 == copy2}")
+        flag = false
+      }
+      catch{
+        case _=>print("Something went wrong Error occurred.")
+      }
+    }
+  }
+}
+
+
+
