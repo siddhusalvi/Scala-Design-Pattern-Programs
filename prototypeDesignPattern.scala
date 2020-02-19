dbbb89fc405ed42d54a887a63b804b6a7e00e0c4
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-19 12:08:20 +0530

    ADDED:Prototype design pattern program

--- prototypeDesignPattern.scala
+++ prototypeDesignPattern.scala
@@ -1,5 +1,5 @@
 /*
-Filename: singleton_thread_safe
+Filename: prototypeDesignPattern
 Created: Siddhesh Salvi
 Change history:19.2.2020 / Siddhesh Salvi
 Created: Siddhesh Salvi
@@ -9,30 +9,26 @@ Use Prototype Pattern as shown in the Link above to create multiple Employee Obj
 */
 
 
-case class Employees(data:List[String])
+case class Employees(data: List[String])
 
 object prototypeDesignPattern {
   def main(args: Array[String]): Unit = {
     var flag = true
-    while(flag){
-      try{
-
-
-        var employees_obj = Employees(List("sunny","vaibhav","siddhu","rinku"))
+    while (flag) {
+      try {
+        var employees_obj = Employees(List("sunny", "vaibhav", "siddhu", "rinku"))
         var copy1 = employees_obj.copy()
         var copy2 = employees_obj.copy()
         var copy3 = employees_obj.copy()
-
-
         System.out.println(s"The prototype is: ")
-        System.out.println(s"Copy is  1: ${copy1}")
-        System.out.println(s"Copy is : ${copy2}")
-        System.out.println(s"Copy is : ${copy3}")
+        System.out.println(s"Copy is 1: $copy1")
+        System.out.println(s"Copy is 2 : $copy2")
+        System.out.println(s"Copy is 3 : $copy3")
         System.out.println(s"1 and 2 are equal: ${copy1 == copy2}")
         flag = false
       }
-      catch{
-        case _=>print("Something went wrong Error occurred.")
+      catch {
+        case _ => print("Something went wrong Error occurred.")
       }
     }
   }
