9c84ecf4b943c12df01feaa132a11fa234bd0d60
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-20 19:08:52 +0530

    ADDED: Exception handling

--- displayAddressBook.scala
+++ displayAddressBook.scala
@@ -1,5 +1,13 @@
+import java.io.FileNotFoundException
+
 import scala.FacadeDesignPattern
 object displayAddressBook extends App {
-  var addressbook = new FacadeDesignPattern()//calling Facade design pattern
-  addressbook.viewContacts()
+  try {
+    var addressbook = new FacadeDesignPattern() //calling Facade design pattern
+    addressbook.viewContacts()
+  }catch {
+    case ex1:FileNotFoundException=>println("File not found exception occurred")
+    case ex2:ClassNotFoundException=>println("Class not found exception occurred")
+    case _=>println("Unknown exception occurred.")
+  }
 }
