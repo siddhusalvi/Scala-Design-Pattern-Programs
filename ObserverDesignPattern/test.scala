783cfa79b44353c558b316b01cc4fab744a75c9f
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-20 12:34:33 +0530

    ADDED: Observer design pattern program

--- ObserverDesignPattern/test.scala
+++ ObserverDesignPattern/test.scala
@@ -1,3 +1,13 @@
+/*
+Filename: AdapterDesignPattern
+Created: Siddhesh Salvi
+Change history:20.2.2020 / Siddhesh Salvi
+Created: Siddhesh Salvi
+3. Prototype design pattern is used when the Object creation is a costly affair and
+requires a lot of time and resources and you have a similar object already existing.
+Use Prototype Pattern as shown in the Link above to create multiple Employee Object
+*/
+
 package ObserverDesignPattern
 
 object test extends App {
